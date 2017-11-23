6bf2ce1f66d3cda2519f0446f7d0d41ad7e4aac2
Gyuhyeon Lee <mark3236@naver.com>
2017-11-24 06:57:27 +0900

    fixed sql cursor typo

--- Package_Aggregator_Gyuhyeon_NodeJS/routes/query.js
+++ Package_Aggregator_Gyuhyeon_NodeJS/routes/query.js
@@ -58,7 +58,7 @@ router.get('/', function(req, res, next) {
             uri: "https://www.google.com/recaptcha/api/siteverify",
             formData: {
                 secret: form.secret,
-                response: req.query.recaptcharesponse
+                response: form.response
             }
         };
         request(options).then((body) => {
@@ -76,13 +76,13 @@ router.get('/', function(req, res, next) {
                             res.json(ret);
                         }
                         else{
-                            connection.query('SELECT * FROM DeliveryLog WHERE trackingnum=?', [req.query.trackingnum], function(err, cur){
-                                if(err != null){
+                            connection.query('SELECT * FROM DeliveryLog WHERE trackingnum=?', [req.query.trackingnum], function(error, cursor){
+                                if(error != null){
                                     console.log("Error when fetching from DB - internal error");
                                 }
                                 else{
                                     if(cursor.length > 0){
-                                        connection.query("UPDATE DeliveryLog SET status=?, history=?, noti=?, phonenum=?", [ret.data.status, ret.data.history, "ON", req.query.phonenum], function(error, cursor){
+                                        connection.query("UPDATE DeliveryLog SET status=?, history=?, noti=?, phonenum=?", [ret.data.status, ret.data.history, "ON", req.query.phonenum], function(err, cur){
                                             if(err != null){
                                                 console.log("Error when fetching from DB - internal error");
                                                 res.json( { success:false, errmsg:"조회중 문제가 발생하였습니다." } );
@@ -93,7 +93,7 @@ router.get('/', function(req, res, next) {
                                         });
                                     }
                                     else{
-                                        connection.query("INSERT INTO DeliveryLog(companycode, trackingnum, sender, receiver, status, history, noti, phonenum) VALUES(?,?,?,?,?,?,?,?)", [ret.data.companycode, ret.data.trackingnum, ret.data.sender, ret.data.receiver, ret.data.status, ret.data.history, "ON", req.query.phonenum], function(error, cursor){
+                                        connection.query("INSERT INTO DeliveryLog(companycode, trackingnum, sender, receiver, status, history, noti, phonenum) VALUES(?,?,?,?,?,?,?,?)", [ret.data.companycode, ret.data.trackingnum, ret.data.sender, ret.data.receiver, ret.data.status, ret.data.history, "ON", req.query.phonenum], function(err, cur){
                                             if(err != null){
                                                 console.log("Error when fetching from DB - internal error");
                                                 res.json( { success:false, errmsg:"조회중 문제가 발생하였습니다." } );

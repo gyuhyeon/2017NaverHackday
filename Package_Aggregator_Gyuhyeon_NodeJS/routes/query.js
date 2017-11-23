a08910f372a472f12d1e4796ac37c066e3a43e81
Gyuhyeon Lee <mark3236@naver.com>
2017-11-24 06:02:20 +0900

    fixed missing library import

--- Package_Aggregator_Gyuhyeon_NodeJS/routes/query.js
+++ Package_Aggregator_Gyuhyeon_NodeJS/routes/query.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request-promise');
 
 const mysql = require('mysql');
 const CJ = require('../crawlerAPI/CJ');
